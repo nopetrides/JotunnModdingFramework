dfbd7c3e22cfdfaef044575ec16a83a1875525d6
Noah Petrides <nopetrides@gmail.com>
2022-10-23 13:17:53 -0700

    Playground Fork
    This is the testing grounds. Solution has been renamed with the powershell script

--- JotunnPlayground/JotunnPlayground.cs
+++ JotunnPlayground/JotunnPlayground.cs
@@ -1,22 +1,22 @@
-﻿// JotunnModStub
-// a Valheim mod skeleton using Jötunn
+// JotunnPlayground
+// a Valheim mod skeleton using J�tunn
 // 
-// File:    JotunnModStub.cs
-// Project: JotunnModStub
+// File:    JotunnPlayground.cs
+// Project: JotunnPlayground
 
 using BepInEx;
 using Jotunn.Entities;
 using Jotunn.Managers;
 
-namespace JotunnModStub
+namespace JotunnPlayground
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
     //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
-    internal class JotunnModStub : BaseUnityPlugin
+    internal class JotunnPlayground : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.jotunn.jotunnmodstub";
-        public const string PluginName = "JotunnModStub";
+        public const string PluginGUID = "com.jotunn.JotunnPlayground";
+        public const string PluginName = "JotunnPlayground";
         public const string PluginVersion = "0.0.1";
         
         // Use this class to add your own localization to the game
@@ -26,10 +26,11 @@ namespace JotunnModStub
         private void Awake()
         {
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
-            Jotunn.Logger.LogInfo("ModStub has landed");
+            Jotunn.Logger.LogInfo("JotunnPlayground has landed");
             
             // To learn more about Jotunn's features, go to
             // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
         }
     }
 }
+
